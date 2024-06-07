fba3cc62b9feb887289752f9d87adc3b88f8f80f
Rashmi Pubuditha <e18268@eng.pdn.ac.lk>
2024-06-07 21:19:41 +0530

    Update V.0.2

--- proj1/Properties/Resources.Designer.cs
+++ proj1/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace proj1.Properties {
+namespace EcontactApp.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace proj1.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("proj1.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EcontactApp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
